cc94b26ea3af66b97c194cb7ccf2804e6a59c3b2
Guilene <guileneerna@yahoo.fr>
2022-11-18 22:29:39 -0500

    nginx

---
+++ nginx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author: devops student
+#Date 11/18/2022
+# ngnix installation
+echo " installation of nginx just wait one min..."
+
+yum update
+yum install nginx -y
+
+echo "complete!!!"
